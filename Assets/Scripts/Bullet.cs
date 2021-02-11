0e389f23723281a462a530f10701ce4b9ba11902
Angel-Lap <angel12345leo@gmail.com>
2021-02-11 13:57:00 +0100

    *

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,13 @@ public class Bullet : MonoBehaviour
 {
     public int damage = 1;
     public float speed = 12;
+    public AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource.volume = audioSource.volume + Random.Range(-0.5f, 0.5f);
+        audioSource.pitch = audioSource.pitch + Random.Range(-0.075f, 0.075f);
+    }
 
     void Update()
     {
